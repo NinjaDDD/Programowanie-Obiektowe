8b3267f30d72b3784ebad1625163ef1ce45fae28
NinjaDDD <40261686+NinjaDDD@users.noreply.github.com>
2023-10-10 15:49:27 +0200

    Add files via upload

---
+++ Zadanie2.cs
@@ -0,0 +1,90 @@
+﻿// Zad 1
+// Console.WriteLine("Zadanie 1");
+// Double c, f;
+// c = double.Parse(Console.ReadLine());
+// f = 32 + 9 / 2 * c;
+// Console.WriteLine(f);
+
+// Zad 2 
+// Console.WriteLine("Zadanie 2");
+// int D, a, b, c;
+// Console.WriteLine("Podaj a");
+// a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Podaj b");
+// b = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Podaj c");
+// c = Convert.ToInt32(Console.ReadLine());
+// D = (int)Math.Pow(b, 2) - 4 * a * c;
+// Console.WriteLine(D);
+
+// Zad 3
+// Console.WriteLine("Zadanie 3");
+// Double m, w, wynik;
+// Console.WriteLine("Podaj mase ciala w kg");
+// m = double.Parse(Console.ReadLine());
+// Console.WriteLine("Podaj wzrost w m");
+// w = double.Parse(Console.ReadLine());
+// wynik = m / Math.Pow(w, 2);
+// Console.WriteLine(wynik);
+
+// Zad 4
+// Console.WriteLine("Zadanie 4");
+// int x = 100;
+// Console.WriteLine(++x * 2);
+
+// Zad 5
+// Console.WriteLine("Zadanie 5");
+// int x = 2, y = 3;
+// x *= y * 2;
+// Console.WriteLine(x = y * 2);
+
+// Zad 6
+// int x, y = 4;
+// x = (y -= 2);
+// x = y++;
+// x = y--;
+// Console.WriteLine("y to: "+ y + ", x to: "+ x);
+
+// Zad 7
+// int x, y = 5;
+// x = ++y * 2;
+// x = y++;
+// x = y--;
+// Console.WriteLine(++y);
+// Console.WriteLine("y to: "+ y + ", x to: "+ x);
+
+// Zad 8
+// bool x;
+// int y = 1, z = 1;
+// x = (y == 1 && z++ == 1);
+// Console.WriteLine(x);
+
+// Zad 9
+/*int x = 1, y = 4, z = 2;
+bool wynik = (x++ > 1 && y++ == 4 && z-- > 0);
+Console.WriteLine("wynik={0} x={1} y={2} z={3}", wynik, x, y, z);
+
+int x = 1, y = 4, z = 2;
+bool wynik = (x++ > 1 & y++ == 4 && z-- > 0);
+Console.WriteLine("wynik={0} x={1} y={2} z={3}", wynik, x, y, z);
+
+int x = 1, y = 4, z = 2;
+bool wynik = (x++ > 1 & y++ == 4 & z-- > 0);
+Console.WriteLine("wynik={0} x={1} y={2} z={3}", wynik, x, y, z);
+
+int x = 1, y = 3, z = 4;
+bool wynik = (x == 1 || y++ > 2 || ++z > 0);
+Console.WriteLine("wynik={0} x={1} y={2} z={3}", wynik, x, y, z);
+
+int x = 1, y = 3, z = 4;
+bool wynik = (x == 1 | y++ > 2 || ++z > 0);
+Console.WriteLine("wynik={0} x={1} y={2} z={3}", wynik, x, y, z);
+
+int x = 1, y = 3, z = 4;
+bool wynik = (x == 1 | y++ > 2 | ++z > 0);
+Console.WriteLine("wynik={0} x={1} y={2} z={3}", wynik, x, y, z);   */
+
+// Zad 10
+// double powierzchnia = 100, osoby = 10;
+// double gestośćZaludnienia = (osoby/powierzchnia);
+// Console.WriteLine(gestośćZaludnienia);
